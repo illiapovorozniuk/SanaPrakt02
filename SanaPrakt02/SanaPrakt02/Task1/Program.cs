6e2a44469cf1a9cef738bc5f6465abf5bb4ad8c7
illia povorozniuk <student.ztu.edu.ua>
2022-11-08 15:00:45 +0200

    5 duplicates

--- SanaPrakt02/SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/SanaPrakt02/Task1/Program.cs
@@ -1,1 +1,6 @@
 ﻿Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
