53ca555e3493cadc4e1537f91269f7d4f67061c5
illia povorozniuk <student.ztu.edu.ua>
2022-11-08 15:05:45 +0200

    change 1-3 strings

--- SanaPrakt02/SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("...");
+Console.WriteLine("...");
+Console.WriteLine("...");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
